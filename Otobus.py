840e8f7f4c9fca83910d230c778d46b2e36cb043
orkuninan <114662514+orkuninan@users.noreply.github.com>
2022-10-06 14:24:25 +0300

    Update Otobus.py

--- Otobus.py
+++ Otobus.py
@@ -1,14 +1,17 @@
 class Otobus:
-    otobusler: str = ""
-    satislar: int = ""
-    iadeler: int = ""
     kalkis: str = ""
     varis: str = ""
+    koltuk_sayi = 0
     dolu_koltik = 0
     plaka: str = ""
-  def bilet_sat(self):
+ def _init_(self, plaka, kalkis, varis, koltuk_sayi):
+        self.plaka= plaka
+        self.kalkis = kalkis
+        self.varis = varis
+        self.koltuk_sayi = koltuk_sayi
+ def bilet_sat(self):
      Otobus.dolu_koltik += 1
  def bilet_iade(self):
      Otobus.dolu_koltik -= 1
  def durum_yaz(self):
-      return f"{self.kalkis},{self.varis},{self.plaka},{self.dolu_koltik}"
+     return f"{self.kalkis},{self.varis},{self.plaka},{self.dolu_koltik}"
